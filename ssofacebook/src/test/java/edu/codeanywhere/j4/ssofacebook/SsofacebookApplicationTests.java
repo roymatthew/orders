5db5cade217ff832755ab5f0f8cb8cb9a0b18c46
rmathewfsb <roy.mathew@flagstar.com>
2017-10-13 17:26:42 -0400

    Initial commit to master

---
+++ ssofacebook/src/test/java/edu/codeanywhere/j4/ssofacebook/SsofacebookApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.codeanywhere.j4.ssofacebook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SsofacebookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
